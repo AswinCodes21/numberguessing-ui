ad6eeb384d229065f935bdd3995b40c20c33faa1
aswins <aswin.sivakumar@tetruscorp.com>
2026-02-02 20:43:44 +0530

    Winscreen fix

--- components/WinScreen.tsx
+++ components/WinScreen.tsx
@@ -22,7 +22,7 @@ const WinScreen: React.FC<Props> = ({
   onHome,
   playerRole = 'NONE',
 }) => {
-  const isWinner = winner === 'SELF';
+const isWinner = winner === playerRole;
   const isHost = playerRole === 'HOST';
 
   return (
